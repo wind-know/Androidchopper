0a2d0c2d698d450c5a9e73299f00cc94aeecf3ea
wind-know <161595828+wind-know@users.noreply.github.com>
2025-09-01 22:41:10 +0800

    first commit customview

--- app/src/main/java/com/example/androidchopper/utils/FlexibleCardView.kt
+++ app/src/main/java/com/example/androidchopper/utils/FlexibleCardView.kt
@@ -1,17 +1,22 @@
 package com.example.androidchopper.utils
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Color
+import android.graphics.Outline
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewOutlineProvider
 import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.cardview.widget.CardView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.constraintlayout.widget.ConstraintSet
 import com.example.androidchopper.R
 
+@SuppressLint("MissingInflatedId")
 class FlexibleCardView @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
@@ -45,6 +50,7 @@ class FlexibleCardView @JvmOverloads constructor(
 
         rootLayout = view.findViewById(R.id.root_constraint)
         iconIv = view.findViewById(R.id.icon_iv)
+
         singleTextTv = view.findViewById(R.id.single_text_tv)
         titleTv = view.findViewById(R.id.title_tv)
         descriptionTv = view.findViewById(R.id.description_tv)
@@ -135,11 +141,11 @@ class FlexibleCardView @JvmOverloads constructor(
                 // 图标居中
                 constraintSet.centerHorizontally(iconIv.id, ConstraintSet.PARENT_ID)
                 constraintSet.centerVertically(iconIv.id, ConstraintSet.PARENT_ID)
+                constraintSet.setVerticalBias(iconIv.id, 0.3f) // 值越小越靠上
                 constraintSet.constrainWidth(iconIv.id, (squareSizeDp * 0.5).dpToPx())
                 constraintSet.constrainHeight(iconIv.id, (squareSizeDp * 0.5).dpToPx())
-
-                // 文字在图标下方
-                constraintSet.connect(singleTextTv.id, ConstraintSet.TOP, iconIv.id, ConstraintSet.BOTTOM, 4.dpToPx())
+//                // 文字在图标下方
+                constraintSet.connect(singleTextTv.id, ConstraintSet.TOP, iconIv.id, ConstraintSet.BOTTOM, 2.dpToPx())
                 constraintSet.connect(singleTextTv.id, ConstraintSet.START, ConstraintSet.PARENT_ID, ConstraintSet.START, 4.dpToPx())
                 constraintSet.connect(singleTextTv.id, ConstraintSet.END, ConstraintSet.PARENT_ID, ConstraintSet.END, 4.dpToPx())
                 constraintSet.connect(singleTextTv.id, ConstraintSet.BOTTOM, ConstraintSet.PARENT_ID, ConstraintSet.BOTTOM)
@@ -152,23 +158,26 @@ class FlexibleCardView @JvmOverloads constructor(
                 descriptionTv.visibility = View.VISIBLE
 
                 // 图标居左，高度撑满
-                constraintSet.connect(iconIv.id, ConstraintSet.START, ConstraintSet.PARENT_ID, ConstraintSet.START, 8.dpToPx())
+                constraintSet.connect(iconIv.id, ConstraintSet.START, ConstraintSet.PARENT_ID, ConstraintSet.START, 2.dpToPx())
                 constraintSet.connect(iconIv.id, ConstraintSet.TOP, ConstraintSet.PARENT_ID, ConstraintSet.TOP, 8.dpToPx())
                 constraintSet.connect(iconIv.id, ConstraintSet.BOTTOM, ConstraintSet.PARENT_ID, ConstraintSet.BOTTOM, 8.dpToPx())
-                constraintSet.constrainWidth(iconIv.id, (smallRectHeightDp * 0.8).dpToPx())
+                constraintSet.constrainWidth(iconIv.id, (smallRectHeightDp * 0.5).dpToPx())
                 constraintSet.constrainHeight(iconIv.id, ConstraintSet.MATCH_CONSTRAINT)
 
-                // 标题
+// 标题
+                constraintSet.constrainWidth(titleTv.id, ConstraintSet.MATCH_CONSTRAINT)
                 constraintSet.connect(titleTv.id, ConstraintSet.START, iconIv.id, ConstraintSet.END, 8.dpToPx())
                 constraintSet.connect(titleTv.id, ConstraintSet.TOP, iconIv.id, ConstraintSet.TOP)
                 constraintSet.connect(titleTv.id, ConstraintSet.END, ConstraintSet.PARENT_ID, ConstraintSet.END, 8.dpToPx())
                 constraintSet.constrainHeight(titleTv.id, ConstraintSet.WRAP_CONTENT)
 
-                // 描述
+// 描述
+                constraintSet.constrainWidth(descriptionTv.id, ConstraintSet.MATCH_CONSTRAINT)
                 constraintSet.connect(descriptionTv.id, ConstraintSet.TOP, titleTv.id, ConstraintSet.BOTTOM, 4.dpToPx())
                 constraintSet.connect(descriptionTv.id, ConstraintSet.START, titleTv.id, ConstraintSet.START)
                 constraintSet.connect(descriptionTv.id, ConstraintSet.END, titleTv.id, ConstraintSet.END)
                 constraintSet.constrainHeight(descriptionTv.id, ConstraintSet.WRAP_CONTENT)
+
             }
 
             Mode.LEFT_TEXT_RIGHT_IMAGE -> {
@@ -196,6 +205,18 @@ class FlexibleCardView @JvmOverloads constructor(
                 constraintSet.connect(descriptionTv.id, ConstraintSet.START, titleTv.id, ConstraintSet.START)
                 constraintSet.connect(descriptionTv.id, ConstraintSet.END, titleTv.id, ConstraintSet.END)
                 constraintSet.constrainHeight(descriptionTv.id, ConstraintSet.WRAP_CONTENT)
+                iconIv.post {
+                    iconIv.applyRoundCorner(radius)
+                }
+
+            }
+        }
+    }
+    private fun ImageView.applyRoundCorner(radius: Float) {
+        this.clipToOutline = true
+        this.outlineProvider = object : ViewOutlineProvider() {
+            override fun getOutline(view: View, outline: Outline) {
+                outline.setRoundRect(0, 0, view.width, view.height, radius)
             }
         }
     }
@@ -209,5 +230,6 @@ class FlexibleCardView @JvmOverloads constructor(
 
     private fun Int.dpToPx(): Int = (this * context.resources.displayMetrics.density + 0.5f).toInt()
     private fun Double.dpToPx(): Int = (this * context.resources.displayMetrics.density + 0.5f).toInt()
-    private fun Float.dpToPx(): Int = (this * context.resources.displayMetrics.density + 0.5f).toInt()
+    private fun Float.dpToPx(): Int =
+        (this * context.resources.displayMetrics.density + 0.5f).toInt()
 }
